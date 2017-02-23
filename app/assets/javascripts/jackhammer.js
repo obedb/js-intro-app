e12ac9d62ef10c9679ea11b16fe6696ea8552e75
Obed Bigirimana <obedbigirimana@gmail.com>
2017-02-23 10:44:38 +0200

    Completed the homework

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,7 @@
 
+
+function jackhammer(){
+  alert("You win!");
+}
+
+
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,1 +1,11 @@
+var winningBox = Math.floor((Math.random()*3)+1)
 
+function three(boxnumber){
+  if (boxnumber === winningBox){
+    alert("You won")
+  } else if (boxnumber < winningBox){
+    alert("Choose A different box")
+  } else {
+    console.log("Wrong")
+  }
+}
