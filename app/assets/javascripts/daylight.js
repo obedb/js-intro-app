e18f03c0e9b7c21bf53a40b92a9a7a278b77815b
Obed Bigirimana <obedbigirimana@gmail.com>
2017-02-24 10:27:54 +0200

    completed the homework

--- app/assets/javascripts/daylight.js
+++ app/assets/javascripts/daylight.js
@@ -1,1 +1,14 @@
 
+function daylight(boxNumber, textBox){
+  if (boxNumber === 1){
+    textBox.style["background-color"] ='yellow';
+
+  } else if (boxNumber === 2){
+    textBox.style["background-color"] = "orange";
+  } else if (boxNumber === 3){
+    textBox.style["background-color"] = "black";
+  }
+  else{
+    console.log("Nothing")
+  }
+}
--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,7 +1,11 @@
 
 
-function jackhammer(){
-  alert("You win!");
-}
+var clickedCount = 0;
+var colors = ["green", "blue", "black", "yellow"];
+function jackhammer(box){
+    box.style["background-color"] = colors[clickedCount];
+    console.log(clickedCount);
+    clickedCount++;
+  }
 
 
